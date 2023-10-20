312aad18cbadaf18644a226e7665cea44fffd009
Sushant <sushantmore09072000@gmail.com>
2023-10-20 14:27:45 +0530

    misc

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -5,7 +5,6 @@ import { Box } from "@mui/material";
 import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
 import Detail from "../components/Detail";
 import ExerciseVideos from "../components/ExerciseVideos";
-import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
